4fa9a247b7782f3e2615f52b11ec653af70f6060
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-10-12 20:57:11 -0400

    Update Lab7-A1.cpp

--- Lab7-A1.cpp
+++ Lab7-A1.cpp
@@ -12,6 +12,7 @@ int main(void) {
 	int numdrunk = 0;
 	int x = 0;
 	int y = 0;
+	int seconds = 120;
 
 	srand(time(0));
 	while (drunk < 10) {
@@ -19,48 +20,47 @@ int main(void) {
 		drunk = numdrunk;
 		cout << "Drunkard #" << drunk;
 
+		for (int i = 1; i <= 10; i++) {
+			for (int i = 0; i <= 8; i++) {
+				int iRand = ((rand() % 9) + 1);
+				movement = iRand;
 
-		for (int m = 0; m <= 8; m++) {
-			movement = rand() % 9;
-		}
 
-		//generate location of x and y coordinate
-		if (movement == 3) {
-			x = 0;
-			y++;
+				//generate location of x and y coordinate
+				if (movement == 3) {
+					y++;
+				}
+				if (movement == 7) {
+					y--;
+				}
+				if (movement == 4) {
+					x++;
+					y++;
+				}
+				if (movement == 5) {
+					x++;
+				}
+				if (movement == 8) {
+					x++;
+					y--;
+				}
+				if (movement == 1) {
+					x--;
+				}
+				if (movement == 2) {
+					x--;
+					y++;
+				}
+				if (movement == 6) {
+					x--;
+					y--;
+				}
+				else if (movement == 0) {
+					x += 0;
+					y += 0;
+				}
+				cout << " finished at location (" << x << ", " << y << ")" << endl;
+			}
 		}
-		if (movement == 7) {
-			x = 0;
-			y--;
-		}
-		if (movement == 4) { 
-			x++;
-			y++;
-		}
-		if (movement == 5) {
-			x++;
-			y = 0;
-		}
-		if (movement == 8) {
-			x++;
-			y--;
-		}
-		if (movement == 1) {
-			x--;
-			y = 0;
-		}
-		if (movement == 2) {
-			x--;
-			y++;
-		}
-		if (movement == 6) {
-			x--;
-			y--;
-		}
-		else if (movement == 0) {
-			x = 0;
-			y = 0;
-		}
-		cout << " finished at location (" << x << ", " << y << ")" << endl;
 	}
 }
